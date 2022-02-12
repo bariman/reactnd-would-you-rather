24b3a3eaa37b44785f2cfdbcea40ac347a873642
Dmitrii <dmitrii.larionov@builtin.com>
2022-02-12 02:02:02 -0600

    feat: Add redux to the app

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+import {createStore} from "redux";
+import reducer from "./reducers";
+import middleware from "./middlewares"
+import { Provider } from 'react-redux'
+
+const store = createStore(
+    reducer,
+    middleware,
+)
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
